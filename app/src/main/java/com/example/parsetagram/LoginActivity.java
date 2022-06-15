ea5b3f6a1beb2da5b28429f1308f95278e986e2e
Tina Wen <tinawen@fb.com>
2022-06-15 09:13:46 -0700

    sign up works

--- app/src/main/java/com/example/parsetagram/LoginActivity.java
+++ app/src/main/java/com/example/parsetagram/LoginActivity.java
@@ -8,11 +8,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -20,6 +24,8 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
+    private Button btnSignup;
+    private TextView tvNoSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +40,9 @@ public class LoginActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignup);
+        tvNoSignup = findViewById(R.id.tvNoSignup);
+
 
         // Listens for a click from the user who is logging in
         btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -43,6 +52,33 @@ public class LoginActivity extends AppCompatActivity {
                 String username = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
                 loginUser(username, password);
+            }
+        });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create the ParseUser
+                ParseUser user = new ParseUser();
+                // Set core properties
+
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+
+                user.setUsername(username);
+                user.setPassword(password);
+                // Invoke signUpInBackground
+                user.signUpInBackground(new SignUpCallback() {
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            // Hooray! Let them use the app now.
+                            loginUser(username, password);
+                        } else {
+                            // Sign up didn't succeed. Look at the ParseException
+                            // to figure out what went wrong
+                        }
+                    }
+                });
             }
         });
     }
